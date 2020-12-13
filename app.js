71b1f07cfd200d9d950869f8728cd236735ed0a1
Ruslan <Alikberov@users.noreply.github.com>
2020-12-14 04:12:33 +0500

    Update app.js
    Test Drive…

--- app.js
+++ app.js
@@ -8,7 +8,78 @@ const	html =
 	,"<html itemscope='' itemtype='http://schema.org/SearchResultsPage' lang='ru'		  >"
 	,"<head											  >"
 	,"<meta content='text/html; charset=utf8'			http-equiv='Content-Type'	 />"
+	,"<meta content='ru-RU'					http-equivconst	http	= require('http');
+const	ipapi	= require('ipapi.co');
+const	hosting	= '127.0.0.1';
+const	port	= 3000;
+
+const	html =
+	["<!doctype html>"
+	,"<html itemscope='' itemtype='http://schema.org/SearchResultsPage' lang='ru'		  >"
+	,"<head											  >"
+	,"<meta content='text/html; charset=utf8'			http-equiv='Content-Type'	 />"
 	,"<meta content='ru-RU'					http-equiv='Content-Language'	 />"
+	,"<meta content='geolocation'						name='keywords'	 />"
+	,"<meta content='Static'							name='googlebot' />"
+	,"<meta content='NoIndex,NoArchive'					name='Robots'	 />"
+	,"<meta content='https://github.com/Alikberov'				name='Author'	 />"
+	,"<head><title>WoBistDu?</title>"
+	,"</head>"
+	,"<body>"
+	,"..."
+	,"</body>"
+	];
+
+var	users	= [];
+
+var callback = function(res){
+	users.unshift("<a href='https://yandex.ru/maps/?z=12&l=map&ll=" + [res.longitude, res.latitude].join() + "'>" + (users.length + 1) + "</a>");
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/html; charset=utf-8");
+	res.end(html.join("\r\n").replace("...", users.join("<br />")));
+};
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+async function my_server(req, res) {
+	////////////////////////////////////////////////////////
+	ipapi.location(callback)       // Complete location for your IP address
+/*	var	requrl	= unescape(req.url.replace(/\+/g, " "));
+	var	szTheme	= "";
+	var	ipAddr	= req.headers["x-forwarded-for"];
+	if(ipAddr) 
+		ipAddr	= ipAddr.split(",").pop();
+	else
+		ipAddr	= req.connection.remoteAddress;
+	var	theIP	= ipAddr.split(/:+/).pop().split(".").join(".");
+	var	fail	= false;
+	var	time	= datefmt(new Date(), Config.timefmt).shifted;
+	//
+	if(!fail) {
+		res.statusCode = 200;
+		res.setHeader("Content-Type", "text/html; charset=utf-8");
+		res.end("")
+	} else {
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'image/png');
+		res.end();
+	}*/
+};
+
+const server = http.createServer(my_server);
+
+server.listen(port, hosting, () => {
+//	log(`Server running at http://${hosting}:${port}/`);
+});
+'Content-Language'	 />"
 	,"<meta content='geolocation'						name='keywords'	 />"
 	,"<meta content='Static'							name='googlebot' />"
 	,"<meta content='NoIndex,NoArchive'					name='Robots'	 />"
