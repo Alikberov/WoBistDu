d430a7570b89324eaf8aac27d772b2ee460c51a1
Ruslan <Alikberov@users.noreply.github.com>
2020-12-20 04:50:11 +0500

    Update index.js
    Actual Changes…

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var	hInterval, msg;
 function geoFindMe() {
 	const status = document.querySelector("#status");
 	const mapLink = document.querySelector("#map-link");
+	const	tracker = document.getElementById("Tracker");
+	var	link;
 	mapLink.href = "";
 	mapLink.textContent = "";
 	function success(position) {
@@ -11,7 +13,9 @@ function geoFindMe() {
 		status.textContent = "";
 		mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
 		mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-		document.getElementById("Tracker").src = `https://wobistdu.herokuapp.com/${longitude}%2C${latitude}`;
+		link = `https://wobistdu.herokuapp.com/${longitude}%2C${latitude}`;
+		if(tracker.src != link)
+			tracker.src = link;
 	}
 	function error() {
 		status.textContent = "Unable to retrieve your location";
@@ -24,4 +28,19 @@ function geoFindMe() {
 		navigator.geolocation.getCurrentPosition(success, error);
 	}
 }
-hInterval = setInterval(geoFindMe, 10000)
+hInterval = setInterval(geoFindMe, 60000)
+function init() {
+	var	videos	= [];
+	var	hFrame	= document.getElementById("Videos");
+	var	i, src;
+	for(i = 1; src = hFrame.getAttribute(`data-src${i}`); ++ i)
+		videos.push(src);
+	var	index	= Math.floor(Math.random() * i);
+	if(index > 0)
+		hFrame.src = "https://www.youtube.com/embed/" + videos[index - 1];
+	//
+	if(window.location.protocol == "http:"
+	|| window.location.pathname != "/"
+	|| window.location.search.length > 1)
+		window.location.href = "https:" + "//" + window.location.hostname;
+}
