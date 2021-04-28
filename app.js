48ee6e2e4660df36d37a26c97000bdcf7373166a
Ruslan <Alikberov@users.noreply.github.com>
2021-04-28 19:37:42 +0500

    Update app.js
    SIGTERM/BeforeExit

--- app.js
+++ app.js
@@ -15,6 +15,29 @@ const	log	= function(...args) {
 			logs = [];
 		}, 1000);
 	};
+
+process.on('SIGTERM', signal => {
+  console.log(`Process ${process.pid} received a SIGTERM signal`)
+  process.exit(0)
+})
+
+process.on('SIGINT', signal => {
+  console.log(`Process ${process.pid} has been interrupted`)
+  process.exit(0)
+})
+
+process.on('beforeExit', code => {
+  // Can make asynchronous calls
+  setTimeout(() => {
+    console.log(`Process will exit with code: ${code}`)
+    process.exit(code)
+  }, 100)
+})
+
+process.on('exit', code => {
+  // Only synchronous calls
+  console.log(`Process exited with code: ${code}`)
+})
 //////////////////////////////////////////////////////////////////////////////
 const	replics	=
 	["Saluto!"
